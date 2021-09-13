cc2d0319c97713150c0aeb50ef7f5b61f9b17ad9
injaepak <85613541+injaepak@users.noreply.github.com>
2021-09-13 17:22:58 +0900

    4.26.2.Ver
    ㅜㅜ 생성 완료입니다

---
+++ Source/Subway/Private/VR_Player.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VR_Player.h"
+
+// Sets default values
+AVR_Player::AVR_Player()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AVR_Player::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVR_Player::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AVR_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
--- Subway/Intermediate/Build/Win64/SubwayEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30133
-#include "C:/Users/Default.DESKTOP-6F3BTUF/Desktop/Subway/Intermediate/Build/Win64/SubwayEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
