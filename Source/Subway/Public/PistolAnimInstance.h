84fda37bf8745dd7ffb59ac78e68c184a7a269ca
ImTaeYeonLee <90662629+ImTaeYeonLee@users.noreply.github.com>
2021-10-06 14:54:07 +0900

    피스톨, 샷건 애니메이션 추가(적용중)

---
+++ Source/Subway/Public/PistolAnimInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PistolAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUBWAY_API UPistolAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+UPROPERTY(VisibleAnywhere, Category = "test", BlueprintReadWrite)
+bool isShooting = false;
+	
+};
